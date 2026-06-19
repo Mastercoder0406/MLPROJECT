01450c4c7b284500c34937cec5618bc77c99b799
Atharva Dhokte <master.atharva04#gmail.com>
2026-06-19 23:32:59 +0530

    Data Tranformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -30,9 +33,10 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            logging.info("Train Test split initaiated")
+           
 
-            #train test set
+            #train test set file creation
+            logging.info("Train Test split initaiated")
             train_set, test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False,header=True)
@@ -50,4 +54,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
